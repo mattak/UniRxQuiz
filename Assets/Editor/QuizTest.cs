8c89889aa8fea91c372879eb1a8a644e6a4ebaaf
mattak <mattak.me@gmail.com>
2017-10-11 16:03:13 +0900

    Initialized

---
+++ Assets/Editor/QuizTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace UniRxQuiz
+{
+    public class QuizTest
+    {
+        [Test]
+        public void Q1()
+        {
+        }
+        
+        [Test]
+        public void Q2()
+        {
+        }
+    }
+}
